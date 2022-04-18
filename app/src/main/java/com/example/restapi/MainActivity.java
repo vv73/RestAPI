1772bf3d133608662e2c3559b9d2d78605cba806
Vladimir Iljin <oivt@ya.ru>
2022-04-18 18:12:21 +0300

    Консольный рабочий пример

--- app/src/main/java/com/example/restapi/MainActivity.java
+++ app/src/main/java/com/example/restapi/MainActivity.java
@@ -3,7 +3,14 @@ package com.example.restapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://api.rasp.yandex.net/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
+        RaspAPI service = retrofit.create(RaspAPI.class);
+
+        Call<Schedule> call = service.nearestStations("v3.0",
+                "269c4d05-3bb8-4c32-bcef-d42e3892c937",
+                55.743692, 37.6822959);
+
+        call.enqueue(new Callback<Schedule>() {
+            @Override
+            public void onResponse(Call<Schedule> call, Response<Schedule> response) {
+                Schedule schedule = response.body();
+                Log.d("RETROFIT", schedule.getStations().get(0).getTitle());
+                TextView tv = findViewById(R.id.textView);
+                tv.setText(Html.fromHtml("<a href=\"https://yandex.ru\">Яндекс</a>"));
+                tv.setMovementMethod(LinkMovementMethod.getInstance());
+            }
+
+            @Override
+            public void onFailure(Call<Schedule> call, Throwable t) {
+                Log.e("RETROFIT", t.getMessage());
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/restapi/RaspAPI.java
+++ app/src/main/java/com/example/restapi/RaspAPI.java
@@ -6,6 +6,9 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RaspAPI {
-    @GET("v3.0/nearest_stations")
-    Call<Schedule> nearestStations(@Query("apikey") String apiKey, @Query("lat") double lat, @Query("lng") double lng);
+    @GET("{version}/nearest_stations")
+    Call<Schedule> nearestStations( @Path("version") String p,
+                                    @Query("apikey") String apiKey,
+                                    @Query("lat") double lat,
+                                    @Query("lng") double lng);
 }
